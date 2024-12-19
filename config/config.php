d2a1352ee11f407db8292b76af257b4afc051db9
Jarvee Vios <128655048+DevJarbs@users.noreply.github.com>
2024-12-19 10:39:19 +0800

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -1,1 +1,9 @@
-
+<?php 
+return [
+       'db_host' => 'localhost',
+       'db_name' => 'eclipse_gym',
+       'db_username' => 'root',
+       'db_password' => '',
+       'charset' => 'utf8',
+];
+?>
